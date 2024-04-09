e7bc06520995423ead3213b96a8a1d7412dcec54
{JhyoonVirtualbox} <{face5921@naver.com}>
2024-04-09 23:07:30 +0900

    add launch file

---
+++ src/pwm_msg/launch/pwm.launch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+   
+    return LaunchDescription([
+        Node(
+            package='pwm_msg',
+            executable='KeyboardInput',
+            name='KeyboardInput',
+            output='screen'),
+        Node(
+            package='pwm_msg',
+            executable='PwmPublisher',
+            name='PwmPublisher',
+            output='screen'),
+    ])
